82a9c62c9b3766ad426009b5fcfe7508f3f4a97a
dlongo <dlongodesign@gmail.com>
2021-12-13 17:06:02 -0500

    fixed index page audio auto play audio

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ const soundOn = document.querySelector("#on")
 const soundOff = document.querySelector("#off")
 themeSong = new Audio("sounds/theme-music.mp3")
 
+function autoPlay() {
+    themeSong.play(); //play the audio file
+    themeSong.volume = 0.4
+}
+
 // homepage theme song
 soundOn.addEventListener("click", () => {
-    themeSong.play()
-    themeSong.volume = 0.4
+    themeSong.pause()
     soundOff.classList.remove("hide")
     soundOn.classList.add("hide")
 })
 
 soundOff.addEventListener("click", () => {
-    themeSong.pause()
+    themeSong.play()
+    themeSong.volume = 0.4
     soundOn.classList.remove("hide")
     soundOff.classList.add("hide")
 })
